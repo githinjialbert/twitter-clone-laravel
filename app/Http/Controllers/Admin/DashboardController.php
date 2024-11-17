eea80aeedcf335cfd97fe5ff2a07fd3c301b78fc
githinjialbert <githinjialbert001@gmail.com>
2024-11-17 21:16:37 +0300

    dealing with the middleware

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
+        if(!Auth::user()->is_admin) {
+            abort(403);
+        }
         return view('admin.dashboard');
     }
 }
---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('before code execution');
+        $response = $next($request);
+        Log::info('after code execution');
+        return $response;
+    }
+}
