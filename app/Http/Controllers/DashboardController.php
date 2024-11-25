2fe6af03d9e8196f53ad2584976d79653aaa7a2f
githinjialbert <githinjialbert001@gmail.com>
2024-11-25 17:24:01 +0300

    initial commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Idea;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
@@ -11,18 +12,20 @@ class DashboardController extends Controller
 
         $ideas = Idea::withCount('likes');
 
-
         if (request()->has('search')) {
             $searchTerm = request()->get('search', '');
             $ideas->where('content', 'like', '%' . $searchTerm . '%');
         }
 
 
+        $topUsers = User::withCount('idea')->orderBy('idea_count', 'DESC')->limit(5);
+
         $ideas = $ideas->orderBy('created_at', 'DESC')->paginate(5);
 
 
         return view("dashboard", [
-            'ideas' => $ideas
+            'ideas' => $ideas,
+            'topUsers' => $topUsers
         ]);
     }
 
